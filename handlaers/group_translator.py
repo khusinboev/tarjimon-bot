cfe889b30ead309af25f2f98c67965ebfd978049
a.husinboyev@gmail.com <a.husinboyev@gmail.com>
2023-09-05 18:11:50 +0500

    yaxshiroq, va xatolar kam

--- handlaers/group_translator.py
+++ handlaers/group_translator.py
@@ -3,6 +3,6 @@ from aiogram import types
 from config import dp
 
 
-@dp.message_handler(chat_type=types.ChatType.SUPERGROUP)
+@dp.message_handler(chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def group_handle(message: types.Message):
     await message.answer(message.as_json())
